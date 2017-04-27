e24567aabd6d1155a970292a09e0bdfcf8c549f5
s-nofc <Miyama@net76-dhcp53.sfc.keio.ac.jp>
2017-04-27 12:39:17 +0900

    junken

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,21 @@
-function action(oppornent){
-  return 1;
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
+
+function action(){
+  return GU;
 }
+
+let matches = 0;
+function action(oppornent){
+  matches = matches + 1;
+  const remainder = matches % 2;
+  const isEven = remainder == 0;
+  console.log(`${matches} % 2 == ${remainder} : ${isEven}`);
+  if(isEven){
+    return GU
+  }
+  return PA;
+}
+
+
